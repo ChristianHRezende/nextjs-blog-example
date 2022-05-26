4982a8fc131372a2f33a83d7612615a085f813da
Sandro Santos - Vortigo <sandro.santos.vortigo@superdigital.com.br>
2022-05-25 23:26:33 -0300

    Head implemented on first post page.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
-    <>
+    <div className="container">
+      <Head>
+        <title>Create Next App - First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </div>
   );
 }
