ab6d85e076666be93fe354b5b5b1b5fa610c626f
Sandro Santos <sandro.santos.vortigo@superdigital.com.br>
2022-06-02 15:21:59 -0300

    2-6-2022 class

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from './../../styles/utils.module.css';
 
 export default function Post({ postData }) {
     return (
@@ -9,12 +10,13 @@ export default function Post({ postData }) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
