70980cf7118bf9bbc7beff01071ab34e0f7b4185
Christian Rezende <christian.rezende@vortigo.com.br>
2022-06-03 15:03:41 -0300

    chore add comments exaplaining fallback

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,39 +1,47 @@
-import Head from 'next/head';
-import Layout from '../../components/layout';
-import Date from '../../components/date';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import utilStyles from './../../styles/utils.module.css';
+import Head from "next/head";
+import Layout from "../../components/layout";
+import Date from "../../components/date";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "./../../styles/utils.module.css";
 
 export default function Post({ postData }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <article>
-                <h1 className={utilStyles.headingXL}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </article>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  const paths = getAllPostIds();
+
+  /*
+    when not found that specific path
+    
+    falback === false =  render 404 not found page 
+    falback === true =  render the component from path without execute getStaticProps
+    falback === blocking = render the component from path executing getStaticProps
+  */
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    return {
-        props: {
-            postData,
-        },
-    };
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
